64bbe13f77f31c414cf4b0e3cd64276fe2bc227e
Kerop1981 <vova.abraamyan@mail.ru>
2025-06-09 21:38:09 +0300

    оставлен только функционал создания канала

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -13,6 +13,6 @@ export class AuthService {
   }
 
   login(email: string, password: string) {
-    return createUserWithEmailAndPassword(this.auth, email,password)
+    return createUserWithEmailAndPassword(this.auth, email,password);
   }
 }
